d309142d3df081c461191796e1b7bd3de5b5cf48
rhyli02 <48325632+rhyli02@users.noreply.github.com>
2020-09-13 01:06:25 +0800

    Sum of Integers in Text File
    MicroPact Exam SUm of Integers
    
    function that takes in a full file path and will return the sum of integers from the text file.

---
+++ MicroPact_Exam_SumIntegersInFile/MicroPact_Exam_SumIntegersInFile/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace MicroPact_Exam_SumIntegersInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            Console.Write("Enter the File Path : ");
+            path = Console.ReadLine();
+            int result = sumIntegersInFile(path);
+            Console.WriteLine("SUM : {0}", result);
+
+        }
+        
+        public static int sumIntegersInFile(string fullFilePath) 
+        {
+            String line;
+            double sum = 0;
+            int count = 0;
+             try
+             {
+                 StreamReader sr = new StreamReader(fullFilePath);
+                 line = sr.ReadLine();
+
+                 while (line != null)
+                 {
+                     foreach (char c in line)
+                     {
+                         if (char.IsDigit(c))
+                         {
+                             sum += Char.GetNumericValue(line, count);
+                         }
+                         count++;
+                     }
+                     count = 0;
+
+                     line = sr.ReadLine();
+                 }
+
+                 sr.Close();
+             }
+             catch (Exception e)
+             {
+                 Console.WriteLine("Exception: " + e.Message);
+             }
+             return Convert.ToInt32(sum);
+            
+        }
+
+    }
+}
